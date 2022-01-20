cfe8c5fb5fd934c01db0b53a35355109860ced7b
Christian Yngvesson <chrille_y@hotmail.com>
2022-01-20 13:46:08 +0100

    Some logic and style...

--- main.js
+++ main.js
@@ -6,29 +6,29 @@ async function getData(file) {
   return data;
 }
 
-async function test() {
-  
-  var test = await getData('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=0&camera=RHAZ&api_key=' + key);
-  console.log(test);
-  var img = test.photos[0].img_src;
+async function Startup() {
+  var CuriosityRHAZ = await getData('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=0&camera=RHAZ&api_key=' + key);
+  console.log(CuriosityRHAZ);
 
+  var img = CuriosityRHAZ.photos[0].img_src;
   console.log(img);
 
   var rover1 = document.getElementById("rover1");
   rover1.src = img;
 }
 
-function Curiosity()
-{
-    
+function getRover(roverName, cameraName) {
+  switch(roverName)
+  {
+    case 'Curiosity':
+        console.log("hello!");
+    break;
+    case 'Opportuniy':
+        console.log("hello 2!");
+    break;
+    case 'Spirit':
+        console.log("hello 3!");
+    break;
+  }
 }
-
-function Opportunity()
-{
-
-}
-function Spirit()
-{
-
-}
-test();
+Startup();
